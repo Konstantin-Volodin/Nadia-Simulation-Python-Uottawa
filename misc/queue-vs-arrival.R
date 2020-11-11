60dc2e74d891b0e24ac84d39bb3b69b1782247d3
unknown <volodin.kostia@gmail.com>
2020-11-11 13:13:25 -0500

    modified results/assumptions

--- misc/queue-vs-arrival.R
+++ misc/queue-vs-arrival.R
@@ -2,6 +2,7 @@ library("zoo")
 library("tidyverse")
 library("here")
 library("sys")
+library("gridExtra")
 
 path <- rstudioapi::getActiveDocumentContext()$path %>% dirname() %>% dirname()
 # Import Data
@@ -23,33 +24,39 @@ raw_single_queue <- raw_single_queue %>% rename(queue = 'Queued To', queue_amoun
   select(-c(queue)) %>% group_by(Replication, Day) %>% summarize(queue = sum(queue_amount)) %>% ungroup()
 
 # Raw Stats
-raw_multi_arrival %>% filter(Day >= 0) %>% 
-  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(arrival))
-raw_single_arrival %>% filter(Day >= 0) %>% 
-  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(arrival))
-raw_multi_queue %>% filter(Day >= 0) %>%
-  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(queue))
-raw_single_queue %>% filter(Day >= 0) %>%
-  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(queue))
+raw_multi_arrival %>% filter(Day >= 2000) %>% 
+  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(arrival)) %>%
+  ungroup() %>% summarise(mean_final = mean(mean), sd_final=sd(mean))
+raw_single_arrival %>% filter(Day >= 2000) %>% 
+  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(arrival)) %>%
+  ungroup() %>% summarise(mean_final = mean(mean), sd_final=sd(mean))
+raw_multi_queue %>% filter(Day >= 2000) %>%
+  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(queue)) %>%
+  ungroup() %>% summarise(mean_final = mean(mean), sd_final=sd(mean))
+raw_single_queue %>% filter(Day >= 2000) %>%
+  select(-c(Day)) %>% group_by(Replication) %>% summarise(mean=mean(queue)) %>%
+  ungroup() %>% summarise(mean_final = mean(mean), sd_final=sd(mean))
 
 # Combines Data
 raw_multi <- inner_join(raw_multi_arrival, raw_multi_queue, by=c('Replication', 'Day')) %>%
   select(-c(Replication)) %>% group_by(Day) %>%
   summarise(mean_arrival = mean(arrival), mean_queue = mean(queue)) %>% ungroup()
+raw_multi$type <- "Multi Queue"
 raw_signle <- inner_join(raw_single_arrival, raw_single_queue, by=c('Replication', 'Day')) %>%
   select(-c(Replication)) %>% group_by(Day) %>%
   summarise(mean_arrival = mean(arrival), mean_queue = mean(queue)) %>% ungroup()
+raw_signle$type <- "Single Queue"
+raw_data <- rbind(raw_multi, raw_signle)
 
 
-ggplot(raw_multi, aes(x=Day)) +
-  geom_line(aes(y=mean_arrival), color='grey40')+
-  geom_line(aes(y=mean_queue/5), color='grey40')+
-  geom_line(aes(y=rollmean(mean_arrival, 180, na.pad=TRUE)), color='dodgerblue4', size=1)+
-  geom_line(aes(y=rollmean(mean_queue/5, 180, na.pad=TRUE)), color='brown4', size=1)+
-  ggtitle("Multi Queue")
-ggplot(raw_signle, aes(x=Day, y=arrival, color=Replication)) +
-  geom_line(aes(y=mean_arrival), color='grey40')+
-  geom_line(aes(y=mean_queue/5), color='grey40')+
-  geom_line(aes(y=rollmean(mean_arrival, 180, na.pad=TRUE)), color='dodgerblue4', size=1)+
-  geom_line(aes(y=rollmean(mean_queue/5, 180, na.pad=TRUE)), color='brown4', size=1)+
-  ggtitle("Single Queue")
+colors <- c("Arrival" = "steelblue2", "Queue/5" = "indianred2", "Mean"="steelblue4")
+ggplot(raw_data, aes(x=Day)) +
+  geom_line(aes(y=mean_arrival), color='grey60')+
+  geom_line(aes(y=mean_queue/5), color='grey60')+
+  geom_line(aes(y=rollmean(mean_arrival, 180, na.pad=TRUE), color="Arrival"), size=1)+
+  geom_line(aes(y=rollmean(mean_queue/5, 180, na.pad=TRUE), color="Queue/5"), size=1)+
+  geom_segment(aes(x = 2000, y = 22.7, yend = 22.7, xend=4320, color="Mean"), linetype="longdash", size=0.9) +
+  geom_vline(xintercept= 2000, colour = "grey30", linetype="dashed") +
+  facet_grid(. ~ type) +
+  ggtitle("Variable Arrival Rate VS Queue Size") + labs(x = "Simulation Day",y = "People",color = "Legend") + 
+  scale_color_manual(values = colors)
